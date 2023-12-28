3d926ebf036cd7cfc03620ee013d54c57aeebf12
deepika.vellingiri <deepika.vellingiri@solutionchamps.com>
2023-12-28 14:15:21 +0530

    Added predefined template features

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,8 @@
-const { configureStore } = require("@reduxjs/toolkit");
-import testimonialReducer from "./slices/testimonialSlice";
-import articleReducer from "./slices/articleSlice";
-import templateReducer from "./slices/templateSlice";
-import careerReducer from "./slices/careerSlice";
-import newsReducer from "./slices/newsSlice";
-import newsLetterReducer from "./slices/newsLetterSlice";
-import pressReleaseReducer from "./slices/pressReleaseSlice";
-import podcastReducer from "./slices/podcastSlice";
-import eventTradeShowsReducer from "./slices/eventTradeShowsSlice";
-import faqReducer from "./slices/faqSlice";
-import contactusReducer from "./slices/contactUsSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
 
 const store = configureStore({
-  reducer: {
-    testimonialData: testimonialReducer,
-    articleData: articleReducer,
-    templateData: templateReducer,
-    careerData: careerReducer,
-    newsData: newsReducer,
-    newsLetterData: newsLetterReducer,
-    pressReleaseData: pressReleaseReducer,
-    podcastData: podcastReducer,
-    eventTradeShowsData: eventTradeShowsReducer,
-    faqData: faqReducer,
-    contactusData: contactusReducer,
-  },
+  reducer: rootReducer,
 });
+
 export default store;
